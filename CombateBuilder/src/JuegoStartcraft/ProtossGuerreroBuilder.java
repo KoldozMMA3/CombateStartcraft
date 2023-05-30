5518510b31dc1282bc51888bca556dbab782a30d
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 22:21:17 -0500

    Conclui con la documentacion de la ultima clase ProtossGuerreroBuilder nuestra tercer raza

--- CombateBuilder/src/JuegoStartcraft/ProtossGuerreroBuilder.java
+++ CombateBuilder/src/JuegoStartcraft/ProtossGuerreroBuilder.java
@@ -1,30 +1,35 @@
 package JuegoStartcraft;
+//CONCRETE BUILDER.
+/**
+ * Clase concreta que implementa el GuerreroBuilder para construir un Guerrero Protoss.
+ * Define los métodos para construir los atributos específicos de un Guerrero Protoss.
+ * 
+ * @author: Cristian Carrizales Luna
+ */
+public class ProtossGuerreroBuilder extends GuerreroBuilder {
 
-public class ProtossGuerreroBuilder extends GuerreroBuilder{
-	public void buildDanio() {
-		guerrero.setDanio(20);
-	}
+    @Override
+    public void buildDanio() {
+        guerrero.setDanio(20);
+    }
 
-	@Override
-	public void buildVida() {
-		guerrero.setVida(400);
-		
-	}
-	
-	public void buildCantidad() {
-		guerrero.setCantidad(1);
-		
-	}
+    @Override
+    public void buildVida() {
+        guerrero.setVida(400);
+    }
+    
+    @Override
+    public void buildCantidad() {
+        guerrero.setCantidad(1);
+    }
 
-	@Override
-	public void buildCosto() {
-		guerrero.setCosto(100);
-		
-	}
+    @Override
+    public void buildCosto() {
+        guerrero.setCosto(100);
+    }
 
-	@Override
-	public void buildNivel() {
-		guerrero.setNivel(1);
-		
-	}
+    @Override
+    public void buildNivel() {
+        guerrero.setNivel(1);
+    }
 }

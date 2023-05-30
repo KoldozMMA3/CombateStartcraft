ede6d85715edb4c4349b0de4c7837432bc46b425
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 20:17:40 -0500

    Ingrese las 2 clases que me permitira construir sus atributos

--- CombateBuilder/src/JuegoStartcraft/ConstruirAtributos.java
+++ CombateBuilder/src/JuegoStartcraft/ConstruirAtributos.java
@@ -1,5 +1,20 @@
 package JuegoStartcraft;
 
 public class ConstruirAtributos {
-
+	private GuerreroBuilder guerreroBuilder;
+	
+	public void setGuerreroBuilder(GuerreroBuilder gb) {
+		guerreroBuilder = gb;
+	}
+	public Guerrero getGuerrero() {
+		return guerreroBuilder.getGuerrero();
+	}
+	
+	public void construirPizza() {
+		guerreroBuilder.crearNuevoGuerrero();
+		guerreroBuilder.builDaño();
+		guerreroBuilder.builVida();
+		guerreroBuilder.builCosto();
+		guerreroBuilder.builNivel();
+	}
 }
--- CombateBuilder/src/JuegoStartcraft/Principal.java
+++ CombateBuilder/src/JuegoStartcraft/Principal.java
@@ -1,5 +1,18 @@
 package JuegoStartcraft;
 
-public class Principal {
+//CLASE PRINCIPAL.
 
+public class Principal {
+public static void main(String[] args) {
+   ConstruirAtributos atributos = new ConstruirAtributos(); // Director.
+   GuerreroBuilder terran_guerrerobuilder = new TerranGuerreroBuilder();
+   GuerreroBuilder zerg_guerrerobuilder = new ZergGuerreroBuilder();
+   GuerreroBuilder protoss_guerrerobuilder = new ProtossGuerreroBuilder();
+
+   atributos.setGuerreroBuilder( terran_guerrerobuilder );
+   atributos.setGuerreroBuilder( zerg_guerrerobuilder );
+   atributos.construirGuerrero();
+
+   Guerrero guerrero = atributos.getGuerrero();
+}
 }

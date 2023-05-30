695da8e3199b18c2a7b5606c885699253eedb0c8
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 20:21:54 -0500

    Implemente set que me pediran los atributos que necesitare

--- CombateBuilder/src/JuegoStartcraft/Guerrero.java
+++ CombateBuilder/src/JuegoStartcraft/Guerrero.java
@@ -1,5 +1,24 @@
 package JuegoStartcraft;
 
 public class Guerrero {
-
+	private String daño = "";
+	private String vida = "";
+	private String costo = "";
+	private String nivel = "";
+	
+	public void setDaño(String daño) {
+		this.daño = daño;
+	}
+	
+	public void setVida(String vida) {
+		this.vida = vida;
+	}
+	
+	public void setCosto(String costo) {
+		this.costo = costo;
+	}
+	
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
 }

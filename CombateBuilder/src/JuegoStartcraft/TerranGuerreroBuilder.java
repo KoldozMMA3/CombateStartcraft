19d476174e579baed08e9c683773c81d627a63fc
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 22:14:47 -0500

    Documentamos la Clase TerranGuerreroBuilder esta sera nuestra primera clase concrete

--- CombateBuilder/src/JuegoStartcraft/TerranGuerreroBuilder.java
+++ CombateBuilder/src/JuegoStartcraft/TerranGuerreroBuilder.java
@@ -1,29 +1,37 @@
 package JuegoStartcraft;
 
+//CONCRETE BUILDER.
+
+/**
+ * Clase concreta que implementa el GuerreroBuilder para construir un Guerrero Terran.
+ * Define los métodos para construir los atributos específicos de un Guerrero Terran.
+ * 
+ * @author: Cristian Carrizales Luna
+ */
 public class TerranGuerreroBuilder extends GuerreroBuilder {
-	public void buildDanio() {
-		guerrero.setDanio(25);
-	}
 
-	@Override
-	public void buildVida() {
-		guerrero.setVida(300);
-		
-	}
-	public void buildCantidad() {
-		guerrero.setCantidad(1);
-		
-	}
+    @Override
+    public void buildDanio() {
+        guerrero.setDanio(25);
+    }
 
-	@Override
-	public void buildCosto() {
-		guerrero.setCosto(75);
-		
-	}
+    @Override
+    public void buildVida() {
+        guerrero.setVida(300);
+    }
 
-	@Override
-	public void buildNivel() {
-		guerrero.setNivel(1);
-		
-	}
+    @Override
+    public void buildCantidad() {
+        guerrero.setCantidad(1);
+    }
+
+    @Override
+    public void buildCosto() {
+        guerrero.setCosto(75);
+    }
+
+    @Override
+    public void buildNivel() {
+        guerrero.setNivel(1);
+    }
 }

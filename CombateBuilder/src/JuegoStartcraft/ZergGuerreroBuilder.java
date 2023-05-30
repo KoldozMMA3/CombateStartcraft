576fd8a076397edf73d648025a609ebaaa2352a7
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 22:18:36 -0500

    Documente la clase ZergGuerreroBuilder esta es la segunda clase Concreta y sera neustra segunda raza de guerrero

--- CombateBuilder/src/JuegoStartcraft/ZergGuerreroBuilder.java
+++ CombateBuilder/src/JuegoStartcraft/ZergGuerreroBuilder.java
@@ -1,30 +1,37 @@
 package JuegoStartcraft;
+//CONCRETE BUILDER.
+/**
+ * Clase concreta que implementa el GuerreroBuilder para construir un Guerrero Zerg.
+ * Define los métodos para construir los atributos específicos de un Guerrero Zerg.
+ * 
+ * @author: Cristian Carrizales
+ */
+public class ZergGuerreroBuilder extends GuerreroBuilder {
 
-public class ZergGuerreroBuilder extends GuerreroBuilder{
-	public void buildDanio() {
-		guerrero.setDanio(10);
-	}
+    @Override
+    public void buildDanio() {
+        guerrero.setDanio(10);
+    }
 
-	@Override
-	public void buildVida() {
-		guerrero.setVida(200);
-		
-	}
-	public void buildCantidad() {
-		guerrero.setCantidad(2);
-		
-	}
+    @Override
+    public void buildVida() {
+        guerrero.setVida(200);
+    }
 
-	@Override
-	public void buildCosto() {
-		guerrero.setCosto(50);
-		
-	}
+    @Override
+    public void buildCantidad() {
+        guerrero.setCantidad(2);
+    }
 
-	@Override
-	public void buildNivel() {
-		guerrero.setNivel(1);
-		
-	}
+    @Override
+    public void buildCosto() {
+        guerrero.setCosto(50);
+    }
+
+    @Override
+    public void buildNivel() {
+        guerrero.setNivel(1);
+    }
 }
+
 

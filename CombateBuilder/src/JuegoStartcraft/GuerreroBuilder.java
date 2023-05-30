44392447bd7bcd528888d8097337791c09e5db5c
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 20:27:55 -0500

    Cree la clase GuerreroBuilder que es la clase Abstracta del proceso Builder este me ayudara a instanciar los procesos Concretos

--- CombateBuilder/src/JuegoStartcraft/GuerreroBuilder.java
+++ CombateBuilder/src/JuegoStartcraft/GuerreroBuilder.java
@@ -1,5 +1,17 @@
 package JuegoStartcraft;
 
 public class GuerreroBuilder {
-
+	protected Guerrero guerrero;
+	
+	public Guerrero getGuerrero() {
+		return guerrero;
+	}
+	
+	public void crearNuevoGuerrero() {
+		guerrero = new Guerrero(); 
+	}
+	public abstract void buildDaño();
+	public abstract void buildVida();
+	public abstract void buildCosto();
+	public abstract void buildNivel();
 }

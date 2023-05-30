feb11fdb27fe5c7e6b26dd4a8b3521943910d7cc
Cristian Carrizales Luna <ccarrizalesl@ulasalle.edu.pe>
2023-05-29 21:53:14 -0500

    Documentamos el Proceso que tenemos en la clase Principal

--- CombateBuilder/src/JuegoStartcraft/Principal.java
+++ CombateBuilder/src/JuegoStartcraft/Principal.java
@@ -1,18 +1,33 @@
 package JuegoStartcraft;
-
+/**
+ * En esta clase Mandaremos a imprimir al guerrero de la raza elegida 
+ * @author Cristian Carrizales Luna
+ **/
 //CLASE PRINCIPAL
 public class Principal {
+    /**
+     * Método principal que se ejecuta al iniciar el programa.
+     * @param args Argumentos de línea de comandos (no se utilizan en este caso).
+     */
     public static void main(String[] args) {
+        // Crear el objeto ConstruirAtributos
         ConstruirAtributos constructor = new ConstruirAtributos(); // Director
-        GuerreroBuilder terran_GuerreroBuilder = new TerranGuerreroBuilder();
-        GuerreroBuilder zerg_GuerreroBuilder = new ZergGuerreroBuilder();
-        GuerreroBuilder protoss_GuerreroBuilder = new ProtossGuerreroBuilder();
 
-        constructor.setGuerreroBuilder(terran_GuerreroBuilder);
+        // Crear los builders para cada raza de guerrero
+        GuerreroBuilder terranGuerreroBuilder = new TerranGuerreroBuilder();
+        GuerreroBuilder zergGuerreroBuilder = new ZergGuerreroBuilder();
+        GuerreroBuilder protossGuerreroBuilder = new ProtossGuerreroBuilder();
 
+        // Establecer el TerranGuerreroBuilder en el constructor
+        constructor.setGuerreroBuilder(terranGuerreroBuilder);
+
+        // Construir los atributos del guerrero
         constructor.construirAtributos();
 
+        // Obtener el guerrero construido
         Guerrero guerrero = constructor.getGuerrero();
-        guerrero.printDatos(); // Ejemplo de uso del guerrero construido
+
+        // Imprimir la información del guerrero
+        System.out.println(guerrero.toString());
     }
 }
